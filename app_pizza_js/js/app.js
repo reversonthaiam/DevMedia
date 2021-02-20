e73b3238bcca56ef483b0de3e5496ee816b39e9f
reversonthaiam <70376757+reversonthaiam@users.noreply.github.com>
2021-02-20 13:26:24 -0300

    envio de pizza pronto

--- app_pizza_js/js/app.js
+++ app_pizza_js/js/app.js
@@ -1,16 +1,13 @@
-var input = document.getElementById("quantidade");
-var botaoIncrementa = document.querySelector("#btn-incrementa")
-var botaoDecrementa = document.querySelector("#btn-decrementa")
 
-botaoIncrementa.addEventListener('click', incrementa)
-botaoDecrementa.addEventListener('click', decrementa)
+//incrementa
+var botoesIncrementa = document.querySelectorAll(".btn-incrementa")
+for(let botao of botoesIncrementa){
 
-
-
+  botao.addEventListener('click', incrementa)
 
 function incrementa(){
   
-  var item = botaoIncrementa.closest('.item');
+  var item = botao.closest('.item');
   var input = item.querySelector('.quantidade')
   input.value++
   
@@ -21,23 +18,52 @@ function incrementa(){
   
 }
 
-function decrementa(){
-  var item = botaoIncrementa.closest('.item');
-  var input = item.querySelector('.quantidade')
+}
 
 
-  if(input.value > 0){
-    input.value--
-    var preco = pegaPrecoItem(item)
-    adicionaAoTotal(-preco)
-  }
 
 
+//decrementa
+
+var botaoDecrementa = document.querySelectorAll(".btn-decrementa")
+
+for(let botao of botaoDecrementa){
   
+  botao.addEventListener('click', decrementa)
 
-
+  function decrementa(){
+    var item = botao.closest('.item');
+    var input = item.querySelector('.quantidade')
+  
+  
+    if(input.value > 0){
+      input.value--
+      var preco = pegaPrecoItem(item)
+      adicionaAoTotal(-preco)
+    }
+  
+  
+  } 
 
 }
+
+var formPedido = document.forms.pedido
+formPedido.addEventListener('submit', function(event){
+  var contador = 0
+  var inputs = formPedido.querySelectorAll("input.quantidade")
+  for(let input of inputs){
+    if(input.value > 0){
+      contador++
+    }
+  }
+  if(contador == 0){
+    alert("Não tem pedidos para serem enviados")
+    event.preventDefault()
+  }
+})
+
+
+
 
 function pegaPrecoItem(item){
   var precoItem = item.querySelector('.preco-item')
